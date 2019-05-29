3d6db486a42bd0ba2e684fe8a6d5dc02bf9c58d6
jon-fearer <fearer.jon@gmail.com>
2019-05-28 20:31:49 -0600

    update files for re-train

--- python/exp_2/predict.py
+++ python/exp_2/predict.py
@@ -1,44 +1,8 @@
 import cv2
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
-from keras import backend as K
+from keras.models import load_model
 
-
-# dimensions of our images.
-img_width, img_height = 150, 150
-
-if K.image_data_format() == 'channels_first':
-    input_shape = (3, img_width, img_height)
-else:
-    input_shape = (img_width, img_height, 3)
-
-model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=input_shape))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Flatten())
-model.add(Dense(64))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
-
-model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
-              metrics=['accuracy'])
-
-model.load_weights('./first_try.h5')
+model = load_model('./model_2.h5')
 img = cv2.imread('new_photo/IMG_1227.jpg')
 img = cv2.resize(img, dsize=(150, 150), interpolation=cv2.INTER_CUBIC)
 img = np.expand_dims(img, axis=0)/255
--- python/exp_2/train.py
+++ python/exp_2/train.py
@@ -12,7 +12,7 @@ train_data_dir = 'data/train'
 validation_data_dir = 'data/validation'
 nb_train_samples = 2000
 nb_validation_samples = 800
-epochs = 10
+epochs = 100
 batch_size = 16
 
 if K.image_data_format() == 'channels_first':
@@ -74,4 +74,4 @@ model.fit_generator(
     validation_data=validation_generator,
     validation_steps=nb_validation_samples // batch_size)
 
-model.save_weights('first_try.h5')
+model.save('model_2.h5')
