dd86c9712261089cbc5f5431068e98230c3047cb
jon-fearer <fearer.jon@gmail.com>
2019-05-30 19:00:20 -0600

    new model run

--- python/exp_2/train.py
+++ python/exp_2/train.py
@@ -14,7 +14,7 @@ train_data_dir = 'data/train'
 validation_data_dir = 'data/validation'
 nb_train_samples = 2000
 nb_validation_samples = 800
-epochs = 250
+epochs = 150
 batch_size = 16
 
 if K.image_data_format() == 'channels_first':
