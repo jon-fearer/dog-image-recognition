36274cb5a8919e3ae654925dc705010eaf3f3931
jon-fearer <fearer.jon@gmail.com>
2019-06-02 21:29:01 -0600

    better validation info

--- python/exp_2/evaluate.py
+++ python/exp_2/evaluate.py
@@ -5,8 +5,8 @@ from keras.preprocessing.image import ImageDataGenerator
 img_width, img_height = 150, 150
 
 validation_data_dir = 'data/validation'
-batch_size = 10
-nb_validation_samples = 10
+batch_size = 100
+nb_validation_samples = 100
 
 test_datagen = ImageDataGenerator(rescale=1. / 255)
 
@@ -17,9 +17,19 @@ validation_generator = test_datagen.flow_from_directory(
     class_mode='binary',
     shuffle=False)
 
-model = load_model('./archive/3/model.h5')
+model = load_model('./archive/4/model.h5')
 result = model.predict_generator(validation_generator,
                                  steps=1)
 
-print(validation_generator.filenames)
-print(result)
+right, wrong = 0, 0
+for loc, item in enumerate(validation_generator.filenames[:100]):
+    print(validation_generator.filenames[loc], result[loc][0])
+    if ((validation_generator.filenames[loc].split('/')[0] == 'melvin' and
+        result[loc][0] < 0.5) or
+        (validation_generator.filenames[loc].split('/')[0] == 'not_melvin' and
+            result[loc][0] >= 0.5)):
+        right += 1
+    else:
+        wrong += 1
+print('right: ', right)
+print('wrong: ', wrong)
