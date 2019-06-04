95020210ab7a8ba96231d2591911094fbd1827e0
jon-fearer <fearer.jon@gmail.com>
2019-06-04 17:48:18 -0600

    fix sort_data script counting

--- python/exp_2/sort_data.py
+++ python/exp_2/sort_data.py
@@ -1,5 +1,4 @@
 import os
-from shutil import copyfile
 
 
 source_melvin = '../../data/melvin/'
@@ -9,13 +8,15 @@ print('Sorting melvin images...')
 melvin_train_count, melvin_validate_count = 0, 0
 for loc, file in enumerate(source_melvin_files):
     src = source_melvin + file
+    file_ext = file.split('.')[-1]
     if loc % 10 == 0:
-        dst = './data/validation/melvin/{}.jpg'.format(loc)
+        dst = './data/validation/melvin/{}.{}'.format(loc, file_ext)
         melvin_validate_count += 1
-    elif loc % 10 != 0:
-        dst = './data/train/melvin/{}.jpg'.format(loc)
+    else:
+        dst = './data/train/melvin/{}.{}'.format(loc, file_ext)
         melvin_train_count += 1
-    copyfile(src, dst)
+    with open(src, 'rb') as fin, open(dst, 'wb') as fout:
+        fout.write(fin.read())
 print('{} images added to melvin train folder'.format(melvin_train_count))
 print('{} images added to melvin validation folder'
       .format(melvin_validate_count))
@@ -28,24 +29,21 @@ print('Number not melvin images: ',
       len(source_not_melvin_files) + len(source_internet_files))
 print('Sorting not melvin images...')
 not_melvin_train_count, not_melvin_validate_count = 0, 0
-for loc, file in enumerate(source_not_melvin_files):
-    src = source_not_melvin + file
+all_not_melvin_files = [source_not_melvin + file for file in
+                        source_not_melvin_files]
+all_not_melvin_files += [source_internet + file for file in
+                         source_internet_files]
+for loc, file in enumerate(all_not_melvin_files):
+    src = file
+    file_ext = file.split('.')[-1]
     if loc % 10 == 0:
-        dst = './data/validation/not_melvin/{}.jpg'.format(loc)
+        dst = './data/validation/not_melvin/{}.{}'.format(loc, file_ext)
         not_melvin_validate_count += 1
-    elif loc % 10 != 0:
-        dst = './data/train/not_melvin/{}.jpg'.format(loc)
+    else:
+        dst = './data/train/not_melvin/{}.{}'.format(loc, file_ext)
         not_melvin_train_count += 1
-    copyfile(src, dst)
-for loc, file in enumerate(source_internet_files):
-    src = source_internet + file
-    if loc % 10 == 0:
-        dst = './data/validation/not_melvin/{}.jpg'.format(loc)
-        not_melvin_validate_count += 1
-    elif loc % 10 != 0:
-        dst = './data/train/not_melvin/{}.jpg'.format(loc)
-        not_melvin_train_count += 1
-    copyfile(src, dst)
+    with open(src, 'rb') as fin, open(dst, 'wb') as fout:
+        fout.write(fin.read())
 print('{} images added to not melvin train folder'
       .format(not_melvin_train_count))
 print('{} images added to melvin validation folder'
