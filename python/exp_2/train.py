bc498f944b874bfc0fe96ef34a0657242b5f81bb
jon-fearer <fearer.jon@gmail.com>
2019-06-05 17:02:21 -0600

    80 epochs to make sure that early stopping kicks in and we get best weights

--- python/exp_2/train.py
+++ python/exp_2/train.py
@@ -17,7 +17,7 @@ train_data_dir = 'data/train'
 validation_data_dir = 'data/validation'
 nb_train_samples = 8000
 nb_validation_samples = 2000
-epochs = 40
+epochs = 80
 batch_size = 16
 
 es = EarlyStopping(monitor='val_acc',
